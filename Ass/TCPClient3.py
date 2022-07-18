9dab566cbd4a731ac9fa08f64cea97162dbf1f04
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2022-07-19 00:32:04 +1000

    initiate ass with login functionality draft

---
+++ Ass/TCPClient3.py
@@ -0,0 +1,31 @@
+from socket import *
+import sys
+
+# Server would be running on the same host as Client
+if len(sys.argv) != 3:
+    print("\n===== Error usage, python3 TCPClient3.py SERVER_IP SERVER_PORT ======\n")
+    exit(0)
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
+serverAddress = (serverHost, serverPort)
+
+# define a socket for the client side, it would be used to communicate with the server
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# build connection with the server and send message to it
+clientSocket.connect(serverAddress)
+
+while True:
+    message = input("Username: ")
+    clientSocket.sendall(message.encode())
+
+    # receive response from the server
+    # 1024 is a suggested packet size, you can specify it as 2048 or others
+    data = clientSocket.recv(1024)
+    receivedMessage = data.decode()
+
+    
+
+
+# close the socket
+clientSocket.close()
